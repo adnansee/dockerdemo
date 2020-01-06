abbb155559d1a45ae0bc8504b8362d85d3be4c29
eddy <adnansee@gmail.com>
2020-01-06 21:26:50 +0100

    bit1

--- src/main/java/com/example/dockerdemo/Addition.java
+++ src/main/java/com/example/dockerdemo/Addition.java
@@ -15,4 +15,24 @@ public class Addition {
 
         return ("Adding "+num1+ " and "+num2+" gives "+Integer.toString(num1+num2));
     }
+
+    @RequestMapping(value = "/")
+    public String welcomeScreen()
+    {
+
+        //return ("--------YAML AUTO--------- SEEMS TO BE WORKING FOLKS --------------------- Click the link   http://localhost:8080/add/2/4 ");
+
+        return ("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "   <head>\n" +
+                "      <title>HTML Image as link</title>\n" +
+                "   </head>\n" +
+                "   <body>\n"+
+"<p> MORDY THE SLEEPY DEVELOPER</p>"+
+                "         <img src=\"https://picsum.photos/1200/1300\"\n" +
+                "         width=1000\" height=\"800\">\n" +
+                "      </a>\n" +
+                "   </body>\n" +
+                "</html>");
+    }
 }
